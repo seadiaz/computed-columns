8e5e42be032536e1963fb5af10540bd847ebc3fe
Sebastian Diaz <seadiaz@gmail.com>
2017-02-28 23:25:20 -0300

    Initialize Kibana Plugin (generator-kibana-plugin v6.2.1)

---
+++ index.js
@@ -0,0 +1,42 @@
+import { resolve } from 'path';
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      
+      app: {
+        title: 'Ratio',
+        description: 'An awesome Kibana plugin',
+        main: 'plugins/ratio/app'
+      },
+      
+      
+      translations: [
+        resolve(__dirname, './translations/es.json')
+      ],
+      
+      
+      hacks: [
+        'plugins/ratio/hack'
+      ]
+      
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+    
+
+  });
+};
---
+++ public/app.js
@@ -0,0 +1,35 @@
+import moment from 'moment';
+import chrome from 'ui/chrome';
+import uiModules from 'ui/modules';
+import uiRoutes from 'ui/routes';
+
+import 'ui/autoload/styles';
+import './less/main.less';
+import template from './templates/index.html';
+
+uiRoutes.enable();
+uiRoutes
+.when('/', {
+  template,
+  resolve: {
+    currentTime($http) {
+      return $http.get('../api/ratio/example').then(function (resp) {
+        return resp.data.time;
+      });
+    }
+  }
+});
+
+uiModules
+.get('app/ratio', [])
+.controller('ratioHelloWorld', function ($scope, $route, $interval) {
+  $scope.title = 'Ratio';
+  $scope.description = 'An awesome Kibana plugin';
+
+  const currentTime = moment($route.current.locals.currentTime);
+  $scope.currentTime = currentTime.format('HH:mm:ss');
+  const unsubscribe = $interval(function () {
+    $scope.currentTime = currentTime.add(1, 'second').format('HH:mm:ss');
+  }, 1000);
+  $scope.$watch('$destroy', unsubscribe);
+});
